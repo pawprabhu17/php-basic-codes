454e142094d7734e81f6e278775034f2ca3483da
pawprabhu17 <pawprabhu17@gmail.com>
2018-09-15 01:25:53 +0530

    PHP code to display table

---
+++ first.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "This is the first index page";
+echo "<br />";
+echo "<br />";
+echo "by:Pawan";
+
+echo "<table>";
+	echo "<tr>";
+		echo "<td>ID</td>";
+		echo "<td>NAME</td>";
+	echo "</tr>";
+	echo "<tr>";
+		echo "<td>16CS067</td>";
+		echo "<td>PAWAN</td>";
+	echo "</tr>";
+echo "</table>";
+
+?>
